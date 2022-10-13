64454fb63fac823482dfe3f1bd551a75d1f1f3d6
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-14 00:05:51 +0300

    Correct linter errors

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -1,29 +1,29 @@
 require './solvers'
 
 describe Solver do
-    context 'When testing factorial method' do
-        it 'Should return 1 when num is 0' do
-            factorial = Solver.new
-            message = factorial.factorial(0)
-            expect(message).to eq 1
-        end
-
-        it 'Should return 1 when num is 1' do
-            factorial = Solver.new
-            message = factorial.factorial(1)
-            expect(message).to eq 1
-        end
-
-        it 'Should return 120 when num is 5' do
-            factorial = Solver.new
-            message = factorial.factorial(5)
-            expect(message).to eq 120
-        end
-
-        it 'Should return Please enter a positive number when num is negative' do
-            factorial = Solver.new
-            message = factorial.factorial(-1)
-            expect(message).to eq 'Please enter a positive number'
-        end
+  context 'When testing factorial method' do
+    it 'Should return 1 when num is 0' do
+      factorial = Solver.new
+      message = factorial.factorial(0)
+      expect(message).to eq 1
     end
+
+    it 'Should return 1 when num is 1' do
+      factorial = Solver.new
+      message = factorial.factorial(1)
+      expect(message).to eq 1
+    end
+
+    it 'Should return 120 when num is 5' do
+      factorial = Solver.new
+      message = factorial.factorial(5)
+      expect(message).to eq 120
+    end
+
+    it 'Should return Please enter a positive number when num is negative' do
+      factorial = Solver.new
+      message = factorial.factorial(-1)
+      expect(message).to eq 'Please enter a positive number'
+    end
+  end
 end
--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,23 +1,23 @@
 require './solvers'
 
 describe Solver do
-    context 'When testing reverse method' do
-        it 'Should return 1 when num is 0' do
-            reverse = Solver.new
-            message = reverse.reverse('hello')
-            expect(message).to eq 'olleh'
-        end
-
-        it 'Should return '' when no inut is given' do
-            reverse = Solver.new
-            message = reverse.reverse('')
-            expect(message).to eq ''
-        end
-
-        it 'Should the string itself when ' do
-            reverse = Solver.new
-            message = reverse.reverse('h')
-            expect(message).to eq 'h'
-        end
+  context 'When testing reverse method' do
+    it 'Should return 1 when num is 0' do
+      reverse = Solver.new
+      message = reverse.reverse('hello')
+      expect(message).to eq 'olleh'
     end
+
+    it 'Should return '' when no inut is given' do
+      reverse = Solver.new
+      message = reverse.reverse('')
+      expect(message).to eq ''
+    end
+
+    it 'Should the string itself when ' do
+      reverse = Solver.new
+      message = reverse.reverse('h')
+      expect(message).to eq 'h'
+    end
+  end
 end
