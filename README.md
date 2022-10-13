49511462f3fd48a4dc471d1c375ac5420df5a337
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-14 00:06:12 +0300

    Update readme file

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # TDD-Project
 Create a class and implement a TDD on the class
+
+## Built With
+ - Ruby
+ - OOP
+
+## Pre-requsite
+- Ruby
+- VS Code
+  
+## Getting Started 
+ To get a local copy of this project:
+  1. Clone the repository (https://github.com/Ney2/TDD-Project.git)
+  2. Change the directory to the cloned repo using cd command
+  3. Open irb terminal
+  4. Run the code
+ That's it! You can play with it as you wish 😄
+
+### Authors
+
+👤 Kidist Dinku
+
+- GitHub: [Ney2](https://github.com/Ney2)
+- Twitter: [@GutaKidist](https://twitter.com/GutaKidist)
+- Linkedin: [Kidist Dinku](https://www.linkedin.com/in/kidist-guta/)
+### Show your support
+Give a ⭐️ if you like this project!
+
+### 🤝 Contributing
+Contributions, [issues](https://github.com/Ney2/TDD-Project/issues), and feature requests are welcome! Feel free to check the issues page.
+
+### Show your support
+Give a ⭐️ if you like this project!
+
+### Acknowledgments
+Thanks to the Microverse team for the great curriculum 🙌.
+Thanks to Code Reviewers for the insightful feedback ⚡💘.
+
+### 📝 License
+This project is [MIT](https://github.com/Ney2/TDD-Project/blob/dev/LICENSE) licensed.
--- solvers.rb
+++ solvers.rb
@@ -1,33 +1,34 @@
 class Solver
-    def factorial(num)
-        if(num<0)
-            return 'Please enter a positive number'
-        elsif(num==0 || num==1)
-            return 1
-        else
-            return num*factorial(num-1)
-        end
+  def factorial(num)
+    if num.negative?
+      'Please enter a positive number'
+    elsif [0, 1].include?(num)
+      1
+    else
+      num * factorial(num - 1)
     end
+  end
 
-    def reverse(string)
-        if(string.length==0)
-            return ''
-        elsif(string.length==1)
-            return string
-        else
-           return string.reverse
-        end
+  def reverse(string)
+    case string.length
+    when 0
+      ''
+    when 1
+      string
+    else
+      string.reverse
     end
+  end
 
-    def fizzbuzz(num)
-        if(num%3==0 && num%5==0)
-            return "FizzBuzz"
-        elsif(num%3==0)
-            return "Fizz"
-        elsif(num%5==0)
-            return "Buzz"
-        else
-            return num.to_s
-        end
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num.to_s
     end
+  end
 end
